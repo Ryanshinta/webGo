77adf2ea15bea7214ee885d9547e4bdea4d32d70
Ryanshinta <Ryanouyangyunru@gmail.com>
2023-04-04 14:40:24 +0800

    implement the tree into router

--- router.go
+++ router.go
@@ -1,29 +1,77 @@
 package webGo
 
 import (
-	"net/http"
+	"strings"
 )
 
 type router struct {
-	handlers map[string]HandlerFunc
+	roots    map[string]*node       //map to store the root nodes of each HTTP method eg, roots['GET'] roots['POST']
+	handlers map[string]HandlerFunc // eg, ['GET-/p/:lang/doc'], handlers['POST-/p/book']
 }
 
 func newRouter() *router {
 	return &router{
+		roots:    make(map[string]*node),
 		handlers: make(map[string]HandlerFunc),
 	}
 }
 
+func parsePattern(pattern string) []string {
+
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' { //Wildcard Pattern Matching
+				break
+			}
+		}
+	}
+
+	return parts
+}
+
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
 }
 
-func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
-	} else {
-		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
 	}
+
+	n := root.search(searchParts, 0)
+
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
 }
