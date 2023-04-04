88f2a1e5a105b127b2352117206df4e5a0404c64
Ryanshinta <Ryanouyangyunru@gmail.com>
2023-04-04 14:09:35 +0800

    complete the trie tree

--- trie.go
+++ trie.go
@@ -1,19 +1,22 @@
 package webGo
 
+import (
+	"strings"
+)
+
 /*
 
 parameter match: /p/:user/detail  can match->/p/userA/detail  ->/p/userB/detail
 
-wildcard match : /static/*  match all the file/path under static.
-
+wildcard match : /static/*  match all the file/path under the path.
 
 */
 
 type node struct {
-	pattern  string // route path
-	part     string
-	children []*node
-	isWild   bool // is
+	pattern  string  // routing path for the node
+	part     string  // current routing section for node
+	children []*node //list of child nodes
+	isWild   bool    // is
 }
 
 func (n *node) matchSingeChild(part string) *node {
@@ -37,6 +40,9 @@ func (n *node) matchAllChildren(part string) []*node {
 	return nodes
 }
 
+// insert a new routing path into node
+// if current node doesn't match the current routing, create a cew child node
+// insert the remainging routing into child node recursively
 func (n *node) insert(pattern string, parts []string, height int) {
 	if len(parts) == height {
 		n.pattern = pattern
@@ -51,4 +57,25 @@ func (n *node) insert(pattern string, parts []string, height int) {
 		n.children = append(n.children, child)
 	}
 	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchAllChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
 }
